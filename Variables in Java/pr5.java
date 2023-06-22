0e5fb0255343376c6e9b4f87054a0b0b96fdb41c
Rudra-Sankha-Sinhamahapatra <rssmp120@gmail.com>
2023-06-22 17:27:23 +0530

    update
    update

---
+++ Variables in Java/pr5.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class pr5 {
+    public static void main(String[] args){
+        //Input
+        Scanner sc=new Scanner(System.in);
+        String name= sc.nextLine();
+        System.out.println(name);
+    }
+}
