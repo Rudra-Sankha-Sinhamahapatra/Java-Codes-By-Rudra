5f1d64f8ca294726f10768fdb98cf6a2c18e9997
Rudra-Sankha-Sinhamahapatra <rssmp120@gmail.com>
2023-06-23 16:54:58 +0530

    update

---
+++ Variables in Java/pr7.java
@@ -0,0 +1,18 @@
+public class pr7 {
+    public static void main(String[] args){
+        
+        int roll=54;
+    
+        float marks=90.87f;
+        double large=88883.4674;
+        long largem=247324737;
+        boolean ur=true;
+        Integer rno=64;
+
+        System.out.println(rno);
+         System.out.println(marks);
+         System.out.println(largem);
+
+    }
+    
+}
---
+++ Variables in Java/pr8.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class pr8 {
+    public static void main(String[] args){
+    Scanner input=new Scanner(System.in);
+    String a=input.nextLine();
+    System.out.println("Name:"+a);
+    System.out.printf("Your name is %s\n",a);
+    }
+    
+}
