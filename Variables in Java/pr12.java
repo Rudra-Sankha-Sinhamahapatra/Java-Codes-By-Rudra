167e510d4f24435e70fde844b4f6210f38d42711
Rudra-Sankha-Sinhamahapatra <rssmp120@gmail.com>
2023-06-23 19:02:04 +0530

    update

---
+++ Variables in Java/pr12.java
@@ -0,0 +1,27 @@
+public class pr12 {
+    public static void main(String[] args){
+        if(true){
+            System.out.println("Hello Woeld!");
+        }
+        if(false){
+            System.out.println("Hii");
+        }
+        int a=10;
+        if(a==10){
+            System.out.println("Hello");
+        }
+        //While loop syntax
+        int count=1;
+        while(count<=5){
+            System.out.println(count);
+            count++;
+        }
+        //For loop Syntax
+        int d;
+        for(d=0;d<=10;d++){
+
+            System.out.println(d);
+        }
+        }
+    
+}
---
+++ Variables in Java/pr13.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class pr13 {
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter celsius");
+        float celsius=input.nextFloat();
+        float fahrenheit=(celsius*9/5)+32;
+        System.out.println("Fahrenheit value:"+fahrenheit+" degree F");
+
+    }
+}
