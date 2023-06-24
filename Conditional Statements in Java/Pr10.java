47ab72d2524d2b32b1c67a23669758f3b0bbe0c7
Rudra-Sankha-Sinhamahapatra <rssmp120@gmail.com>
2023-06-25 04:28:37 +0530

    update

--- Conditional Statements in Java/Pr10.java
+++ Conditional Statements in Java/Pr10.java
@@ -2,8 +2,63 @@
 /* +,-,*,/,% and X for exit
  * Make a menu driven program
  * on the infinite loop while(true)
- * char op=in.next().trim().charAt(0);
+ * char op=input.next().trim().charAt(0);
  */
+import java.util.Scanner;
 public class Pr10 {
+    public static void main(String[] args) {
+    Scanner input=new Scanner(System.in);
+    int ans=0;
+      while(true){
+    System.out.println("Press +,-,*,/,% to do operations and X to exit the operations\n");
+    char op=input.next().trim().charAt(0);
     
+        if(op=='+'|| op=='-'|| op=='*' || op=='/' || op=='%'){
+     System.out.println("Enter two integers\n");
+    int num1=input.nextInt();
+    int num2=input.nextInt();
+    System.out.println();
+        if(op=='+'){
+      ans=num1+num2;
+      
+        }
+        
+         if(op=='-'){
+      ans=num1-num2;
+      
+        }
+         if(op=='*'){
+      ans=num1*num2;
+      
+        }
+        if(op=='%'){
+            ans=num1%num2;
+    
+        }
+        if(op=='/'){
+            if(num2!=0){
+                ans=num1/num2;
+            
+            }
+            else{
+                System.out.println("2nd number cant be 0");
+            
+            }
+        }
+    }
+        else if(op=='X'||op=='x'){
+            System.out.println("Exiting the program");
+        break;
+        }
+        
+    
+     else{
+            System.out.println("Invalid");
+        }
+        System.out.println(ans);
+    }
+    
+    input.close();
+    }
+
 }
