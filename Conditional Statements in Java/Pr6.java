f2d4ffe10e2f82d4e3bed05195cc15780830140c
Rudra-Sankha-Sinhamahapatra <rssmp120@gmail.com>
2023-06-24 22:17:26 +0530

    update
    update

---
+++ Conditional Statements in Java/Pr6.java
@@ -0,0 +1,26 @@
+//Fibonacci numbers program using for loop
+import java.util.Scanner;
+public class Pr6{
+    public static void main(String[] args){
+    Scanner input=new Scanner(System.in);
+    System.out.println("Enter the nth term");
+    int n=input.nextInt();
+    int first=0;
+    int second=1;
+    int fib;
+    System.out.printf("The %dth fibonacci numbers are:",n);
+    System.out.println(" ");
+    System.out.println(first);
+    System.out.println(second);
+   for(int i=1;i<n-1;i++){
+    fib=first+second;
+    first=second;
+    second=fib;
+    System.out.println(fib);
+   }
+    
+      
+    }
+  
+   
+    }
---
+++ Conditional Statements in Java/Pr7.java
@@ -0,0 +1,25 @@
+//Fibonacci numbers program using while loop
+import java.util.Scanner;
+public class pr7 {
+     public static void main(String[] args){
+    Scanner input=new Scanner(System.in);
+    System.out.println("Enter the nth term");
+    int n=input.nextInt();
+    int first=0;
+    int second=1;
+    int fib;
+    System.out.printf("The %dth fibonacci numbers are:",n);
+    System.out.println(" ");
+    System.out.println(first);
+    System.out.println(second);
+    
+    int i=3;
+    while(i<=n){
+      fib=first+second;
+    first=second;
+    second=fib;
+    i++;
+System.out.println(fib);
+    }
+}
+}
