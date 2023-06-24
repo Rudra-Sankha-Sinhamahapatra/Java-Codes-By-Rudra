3572773cae5c17f5caedf1b4d96f9dca944b7fa0
Rudra-Sankha-Sinhamahapatra <rssmp120@gmail.com>
2023-06-25 02:09:13 +0530

    update

---
+++ Conditional Statements in Java/Pr10.java
@@ -0,0 +1,9 @@
+//Calculator program using if else
+/* +,-,*,/,% and X for exit
+ * Make a menu driven program
+ * on the infinite loop while(true)
+ * char op=in.next().trim().charAt(0);
+ */
+public class Pr10 {
+    
+}
---
+++ Conditional Statements in Java/Pr9.java
@@ -0,0 +1,14 @@
+//Reverse a Number
+public class Pr9 {
+    public static void main(String[] args){
+        int num=23597;
+        int rev=0;
+        while(num>0){
+            int rem=num%10;
+            num/=10;
+          rev=(rev*10)+rem;
+        }
+        System.out.println("Reverse:"+rev);
+    }
+    
+}
