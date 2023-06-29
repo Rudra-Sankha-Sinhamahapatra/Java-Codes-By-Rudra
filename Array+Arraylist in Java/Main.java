71dcfb6de9f77ed301424ccbac98aaabd78c4771
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2023-06-30 04:25:14 +0530

    update array

---
+++ Array+Arraylist in Java/Main.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public  class Main{
+    public static void main(String[] args) {
+       //Array Syntax
+       //datatype[] variable_name=new datatype[size]; 
+       //storing 5 roll numbers
+       //int[]roll=new int[5];
+       //or
+       int[]roll1={23,15,3,56,789};
+       System.out.println(Arrays.toString(roll1));
+    }
+}
