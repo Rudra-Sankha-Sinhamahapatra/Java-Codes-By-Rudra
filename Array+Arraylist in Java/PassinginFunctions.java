e049362303147776ed30d90e34e4ea797c769d49
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2023-06-30 05:48:53 +0530

    Updated till passing functions

--- Array+Arraylist in Java/PassinginFunctions.java
+++ Array+Arraylist in Java/PassinginFunctions.java
@@ -1,5 +1,14 @@
 
-
+import java.util.*;
 public class PassinginFunctions {
-    
+    public static void main(String[] args) {
+        int []nums={3,4,4,44};
+        System.out.println(Arrays.toString(nums));
+        change(nums);
+         System.out.println(Arrays.toString(nums));
+    }
+    static void change(int[]arr){
+        arr[0]=99;
+    }
 }
+//Strings are immutable in java but arrays are mutable
