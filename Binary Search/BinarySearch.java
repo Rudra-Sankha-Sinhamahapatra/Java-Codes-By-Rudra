2e5ad6ecf52800257dc635488d1bf66e3275daf5
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2023-07-06 16:21:06 +0530

    update

--- Binary Search/BinarySearch.java
+++ Binary Search/BinarySearch.java
@@ -24,6 +24,7 @@ public class BinarySearch{
     static int binarySearch(int[]arr,int target){
         int start=0;
         int end=arr.length-1;
+        
         while(start<=end){
             //find thr middle element
             int mid=start+(end-start)/2;
--- Binary Search/OrderAgnostic.java
+++ Binary Search/OrderAgnostic.java
@@ -5,7 +5,7 @@ public class OrderAgnostic {
          Scanner in=new Scanner(System.in);
         System.out.println("Enter the size of the array");
         int n=in.nextInt();
-        System.out.println("Enter the elements of the array in desceding order");
+        System.out.println("Enter the elements of the array");
         int[]nums=new int[n];
         for (int i = 0; i < n; i++) {
             nums[i]=in.nextInt();
@@ -25,18 +25,28 @@ public class OrderAgnostic {
     static int orderAgnostic(int[]arr,int target){
         int start=0;
         int end=arr.length-1;
-      while(start<=end){
+        boolean isAsc=arr[start]<arr[end];
+        while(start<=end){
         int mid=start+(end-start)/2;
-        if(target>arr[mid]){
+        if(arr[mid]==target){
+            return target;
+        }
+        if(isAsc){
+      
+        if(target<arr[mid]){
             end=mid-1;
         }
-        else if(target<arr[mid]){
+        else{
             start=mid+1;
         }
-        else{
-            return mid;
+    } else{
+        if(target>arr[mid]){
+            end=mid-1;
+        }else{
+            start=mid+1;
         }
-      }
-      return -1;
+    }
+}
+return -1;
     }
 }
