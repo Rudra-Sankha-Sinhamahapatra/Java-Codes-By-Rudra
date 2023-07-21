b19cac223f9e290d2cd40f18b1fabdb995539d7a
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2023-07-21 14:00:07 +0530

    updated

---
+++ Strings and Stringbuilder/PreetyPrinting.java
@@ -0,0 +1,11 @@
+// package Strings and Stringbuilder;
+
+public class PreetyPrinting {
+    public static void main(String[] args) {
+        float a=423.4371f;
+        System.out.printf("Formatted Number is :%.2f\n",a);
+
+        System.out.printf("Value of Pie is :%.3f\n",Math.PI) ;
+    
+    }
+}
