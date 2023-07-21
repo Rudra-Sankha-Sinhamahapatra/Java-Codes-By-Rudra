4a8b09ab28ef76b20acf8fb68bbf1e3843ec1a2c
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2023-07-21 12:35:19 +0530

    update

---
+++ Strings and Stringbuilder/Comparison.java
@@ -0,0 +1,25 @@
+// package Strings and Stringbuilder;
+
+public class Comparison {
+   public static void main(String[] args) {
+    String a="Rudra";
+    String b="Rudra";
+    //==
+    System.out.println(a==b);//true
+    String c=new String("Kunal");
+    String d=new String("Kunal");
+
+    System.out.println(c==d);//false
+
+    System.out.println(c.equals(d));//true:only checking values
+
+      String e=c;
+    System.out.println(e==c);//true
+
+    String f=new String("Kunal");
+    String g=new String(" Kunal");
+
+     System.out.println(f.equals(g));//false:because of extra space in g the values are now different
+   
+    } 
+}
---
+++ Strings and Stringbuilder/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args) {
+        String name="India";
+        System.out.println(name);
+        name="bharat";
+     System.out.println(name);
+    }
+}
