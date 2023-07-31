8911d0cb74bd0b14adaf4fa09898ffd2e7174c02
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2023-07-31 09:14:10 +0530

    updated

---
+++ Time Complexity/Fibonacci.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Fibonacci{
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter the nth(index) fibonacci Number you want to search for:");
+        int f=in.nextInt();
+        System.out.println("The "+(f+1)+"th Fibonacci Number is "+fiboFormula(f));
+        in.close();
+    }
+    static int fiboFormula(int n){
+         return (int)((Math.pow(((1+Math.sqrt(5))/2),n)-Math.pow(((1-Math.sqrt(5))/2),n))/ Math.sqrt(5));
+    }
+
+}
