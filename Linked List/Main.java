3581b85ca833961362b90f9f5cc819a58b591c20
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2023-08-26 13:05:43 +0530

    updated

--- Linked List/Main.java
+++ Linked List/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
    LinkedList<Integer> list=new LinkedList<>();
    list.add(34);
+   list.add(24);
    System.out.println(list);
     }
 }
